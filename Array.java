137fc35ff05e644ed52b146b674f08d51f207435
AnitaMadbhavi <anitamadbhavi@gmail.com>
2021-12-16 13:01:07 +0530

    today will uploading file

---
+++ Array.java
@@ -0,0 +1,8 @@
+class Array{
+	public static void main(String []args){
+	System.out.println(b);
+	System.out.println(a[1]);
+	}
+	static String[] a={"Sita","Ram","Ravan","Hanuman","Lanka"};
+	static String b="Ramayan";
+}
